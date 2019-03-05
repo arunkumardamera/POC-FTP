b31b21a7b946b42490b05bed5b591a99eba05f74
arunkumardamere <arunkumar.damera18@gmail.com>
2019-03-05 10:08:17 -0800

    by Arun

---
+++ ftp_libtest.py
@@ -0,0 +1,50 @@
+from ftplib import FTP
+import time
+from dateutil import parser
+
+class FTPsession():
+    def __init__(self,
+                 hostname="",
+                 username="",
+                 password=""):
+        print("Ftp Login Started...")
+        self.ftp=None
+        try:
+            self.ftp= FTP(hostname)
+            self.ftp.login(username,password) 
+        except:
+            print("Ftp Login Failed")
+            
+    def get_latest_file_name(self):
+        data = []
+        if self.ftp:
+            #self.ftp.dir('-t',data.append)
+            self.ftp.dir("",data.append)
+            latest_time = None
+            latest_name = None
+            
+            for line in data:
+                tokens = line.split()
+                time_str = tokens[5] + " " + tokens[6] + " " + tokens[7]
+                # date utils are used here to parse the time string
+                time = parser.parse(time_str)
+                if (latest_time is None) or (time > latest_time):
+                    latest_name = tokens[8]
+                    latest_time = time
+            self.latest_file=latest_name
+            return latest_name
+    def download_latest_file(self):
+        fpath = '/opt/' + self.latest_file
+        handle = open(fpath, 'wb')
+        try:
+            self.ftp.retrbinary('RETR %s' % self.latest_file, handle.write)
+        except:
+            print("Ftp download Failed")
+            handle.close()
+            return False
+        return True
+        
+            
+if  __name__=='__main__':
+    ftp1= FTPsession('ftp.pfainc.com','CoreLogic',"#C0r3106!c@m@d0r!")
+    print(ftp1.get_latest_file_name())
---
+++ maintest.py
@@ -0,0 +1,64 @@
+from ftp_lib import FTPsession
+import shutil
+import threading
+import time
+import os
+import subprocess
+
+class ftpTask (threading.Thread):
+    def __init__(self, threadID, name, host, username,password):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name # county name
+        self.host = host
+        self.username=username
+        self.password = password
+
+    def run(self):
+        print ("Starting tasks for " + self.name )
+        time.sleep(3)
+        ftp1 = FTPsession(self.host, self.username , self.password)
+        latest_file = ftp1.get_latest_file_name()
+        # Not needed for the first time
+        rmtar = 'rm -rf /opt/' + self.name + '/*'
+        if not os.path.exists('/opt/' + self.name):
+            result = subprocess.check_output(rmtar, shell=True)
+            print(result)
+        if(ftp1.download_latest_file()):
+            fpath = '/opt/' + latest_file
+            os.chdir('/opt/')
+            if not os.path.exists(self.name):
+                os.makedirs(self.name)
+            dpath= '/opt/' + self.name
+            shutil.move(fpath,dpath)
+            untarcmd = 'tar -xvf ' + dpath + '/*.tar* -C /opt/' + self.name + '/'
+            result = subprocess.check_output(untarcmd, shell=True)
+            print(result)
+        else:
+            #Send out the mail we should return an error
+            print("File Download Fails..")
+    
+def main():
+    
+    threads = []
+    timeoutval = 300
+
+    #In production code the values will be taken out from db
+    #DB coulmns will be county (ca_amador will be unique), type (ftp/email/web), host, username, password
+    #Seperate script will be provided to update db params
+    #Below line create a new thread for ca_amador ftp
+    t_id = ftpTask(1,"ca_amador",'ftp.pfainc.com','CoreLogic',"#C0r3106!c@m@d0r!")
+
+    time.sleep(1)
+    #in production code these two will be done inside a loop
+    if t_id:
+        print('Starting...')
+        t_id.start()
+        time.sleep(1)
+        threads.append(t_id)
+        
+    for t in threads:
+        t.join(timeout=timeoutval)
+
+if __name__ == "__main__":
+    main()
